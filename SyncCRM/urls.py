3ba62f0e227afcc8ca13c7959e3f656b86e8f386
Ahmed Javed <ahmedjaved701@gmail.com>
2020-07-13 19:28:59 +0500

    Bug fixes

--- SyncCRM/urls.py
+++ SyncCRM/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 app_name="users"
 urlpatterns = [
-    path('leads/', include("leads.urls")),
-    path('', include("users.urls")),
+    path('/', include("leads.urls")),
+    path('/', include("users.urls")),
     path('login/', auth_views.LoginView.as_view(template_name="users/login.html"), name="login"),
     path('logout/', auth_views.LogoutView.as_view(template_name="users/logout.html"), name="logout"),
     path('password-reset/', auth_views.PasswordResetView.as_view(template_name="users/password_reset.html"), name="password_resest"),
