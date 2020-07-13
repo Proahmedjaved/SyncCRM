f09b30cef0eeb23a42aeaf8fdc3760a10759deed
Ahmed Javed <ahmedjaved701@gmail.com>
2020-07-13 19:57:23 +0500

    updated settings

--- SyncCRM/settings.py
+++ SyncCRM/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '-wm)&*^+#yv3d8e7xzgt^3%a&qtld%s9gslzi6vdrby#uiim76'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['pacific-tor-59294.herokuapp.com']
 
 
 # Application definition
