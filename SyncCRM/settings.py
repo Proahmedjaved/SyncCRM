6096ffa7438568f1c99eadad407d0feb8b915c44
Ahmed Javed <ahmedjaved701@gmail.com>
2020-07-13 20:06:27 +0500

    bug fixes

--- SyncCRM/settings.py
+++ SyncCRM/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-wm)&*^+#yv3d8e7xzgt^3%a&qtld%s9gslzi6vdrby#uiim76'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
